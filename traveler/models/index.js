01ea450caef59a4e7b8094569096fe28ed230dd8
James Keith <jamiek531@gmail.com>
2018-06-14 20:15:00 -0400

    Added controller and api Routes

--- traveler/models/index.js
+++ traveler/models/index.js
@@ -1,3 +1,8 @@
 module.exports = {
-  Book: require("./book")
+  Book: require("./book"),
+  Activity: require("./activity"),
+  Shelter: require("./shelter"),
+  Trip: require("./trip"),
+  TripLeg: require("./tripLeg"),
+  User: require("./user")
 };
--- traveler/routes/api/index.js
+++ traveler/routes/api/index.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
+const tripRoutes = require("./trips");
+const tripLegRoutes = require("./tripLegs");
+const userRoutes = require("./users");
 
 // Book routes
 router.use("/books", bookRoutes);
+router.use("/trips", tripRoutes);
+router.use("/tripLeg", tripLegRoutes);
 
 module.exports = router;
