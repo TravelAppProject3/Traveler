63bcba7c4f968853be6afaff18376287022ba45a
Brandon Harrington <bh68484@gmail.com>
2018-06-14 20:42:35 -0400

    Added some dependencies and passport code.

---
+++ traveler/config/keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+  google: {
+    clientID:
+      "182270810559-lcme3get66u8l2rivhh3r6du7j25ome4.apps.googleusercontent.com",
+    clientSecret: "yPrInzMQwWRUixvQHO6RGz00"
+  },
+  mongodb: {
+    dbURI: "mongodb://brandon:brandon1@ds159840.mlab.com:59840/travelerapp"
+  },
+  session: {
+    //random string used to encrypt cookie key
+    cookieKey: "kfhhefjhewfhakhfkah"
+  }
+};
