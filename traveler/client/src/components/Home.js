181c96d2f8c524a5e4ba407231b4fc6c428c8271
David <dstillac@ncsu.edu>
2018-06-15 20:28:46 -0400

    Front End Update

--- traveler/client/src/components/Home.js
+++ traveler/client/src/components/Home.js
@@ -1,32 +1,63 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 const styles = {
   root: {
-    backgroundImage: 'url(https://images.pexels.com/photos/1010657/pexels-photo-1010657.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)',
-    backgroundSize: 'cover',
-    overflow: 'hidden',
-    height: '900px',
+    backgroundImage:
+      "url(https://i.pinimg.com/originals/cc/a5/02/cca5022c86f67861746d7cf2eb486de8.gif)",
+    backgroundSize: "cover",
+    overflow: "hidden",
+    height: "720px",
     marginTop: "-40px",
-    // width: '500px',
-    // height: '500px'  
-    textAlign: 'center'
+    textAlign: "center",
+    width: "100%"
   },
-  button: {
-    marginTop: '30px',
-    width: '7%'
+  button1: {
+    // marginTop: "330px",
+    width: "220px",
+    height: "60px",
+    marginRight: "50px",
+    fontSize: "30px",
+    color: "white"
+  },
+  button2: {
+    // marginTop: "330px",
+    height: "60px",
+    fontSize: "30px",
+    width: "220px"
+  },
+  color: {
+    color: "white",
+    textDecoration: "none"
+  },
+  inspiration: {
+    color: "black",
+    marginTop: "200px",
+    marginBottom: "40px",
+    fontSize: "40px",
+    fontWeight: "bold"
   }
 };
-// background: ("https://images.pexels.com/photos/1010657/pexels-photo-1010657.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"),
-
 
 class Home extends Component {
   render() {
     return (
       <div style={styles.root}>
-  
-        <button style={styles.button} className="btn btn-dark">Button</button>
+        <div style={styles.inspiration}>
+          {" "}
+          Take solo travel to the next level with Travelo.
+        </div>
+        <button style={styles.button1} className="btn btn-dark">
+          <Link to="/NewUser" style={styles.color}>
+            Sign Up
+          </Link>
+        </button>
+        <button style={styles.button2} className="btn btn-dark">
+          <Link to="/SignIn" style={styles.color}>
+            Sign In
+          </Link>
+        </button>
       </div>
-      
     );
   }
 }
--- traveler/client/src/components/SignIn/SignIn.js
+++ traveler/client/src/components/SignIn/SignIn.js
@@ -1,24 +1,31 @@
 import React, { Component } from "react";
-import "./style.css"
+import "./style.css";
+
+const styles = {
+  body: {
+    textAlign: "center",
+    backgroundImage:
+      "url(https://i.pinimg.com/originals/cc/a5/02/cca5022c86f67861746d7cf2eb486de8.gif)",
+    backgroundSize: "cover",
+    overflow: "hidden",
+    height: "720px",
+    marginTop: "-40px",
+    textAlign: "center",
+    width: "100%"
+  },
+  margin: {
+    marginTop: "50px"
+  }
+};
 
 class SignIn extends Component {
-  state = {
-    
-  };
-
-  styles = {
-    
-    body: {
-        textAlign: "center"
-    }
-    
-  };
+  state = {};
 
   render() {
     return (
-      <div>
+      <div style={styles.body}>
         <div className="container">
-          <form action="/action_page.php">
+          <form action="/action_page.php" style={styles.margin}>
             <div className="row">
               {/* <h2 style={this.styles.body} >Login with Social Media or Manually</h2> */}
               <div className="vl">
@@ -27,13 +34,13 @@ class SignIn extends Component {
 
               <div className="col">
                 <a href="#" className="fb btn">
-                  <i className="fa fa-facebook fa-fw"></i> Login with Facebook
+                  <i className="fa fa-facebook fa-fw" /> Login with Facebook
                 </a>
                 <a href="#" className="twitter btn">
-                  <i className="fa fa-twitter fa-fw"></i> Login with Twitter
+                  <i className="fa fa-twitter fa-fw" /> Login with Twitter
                 </a>
                 <a href="#" className="google btn">
-                  <i className="fa fa-google fa-fw"></i> Login with Google+
+                  <i className="fa fa-google fa-fw" /> Login with Google+
                 </a>
               </div>
 
@@ -42,15 +49,23 @@ class SignIn extends Component {
                   <p>Or sign in manually:</p>
                 </div>
 
-                <input type="text" name="username" placeholder="Username" required></input>
-                <input type="password" name="password" placeholder="Password" required></input>
-                <input type="submit" value="Login"></input>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  required
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
+                <input type="submit" value="Login" />
               </div>
-
             </div>
           </form>
         </div>
-
       </div>
     );
   }
