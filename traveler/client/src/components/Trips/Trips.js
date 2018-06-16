34438369424e796e04e81c6ac918a4dd6f24a140
Rich <rchrdholme@gmail.com>
2018-06-16 12:13:12 -0400

    trips page

---
+++ traveler/client/src/components/Trips/Trips.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Trips extends Component {
+  state = {};
+
+  styles = {
+    
+  };
+
+  render() {
+    return (
+        <div></div>
+    );
+  }
+}
+
+export default Trips;
