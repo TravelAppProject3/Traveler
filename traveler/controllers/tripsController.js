b288923531ce57127097e00d5e8e4521903711b1
James Keith <jamiek531@gmail.com>
2018-06-20 17:34:44 -0400

    Change to API routes

--- traveler/controllers/tripsController.js
+++ traveler/controllers/tripsController.js
@@ -21,13 +21,18 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Trip.findOneAndUpdate({ _id: req.params.id }, req.body)
+    db.Trip.findOneAndUpdate({ _id: req.params.tripId }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
     db.Trip.findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findByUser: function(req, res) {
+    db.Trip.find({ tripUser: req.params.userId })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
