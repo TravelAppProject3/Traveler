071479b8bc445957ff521060028e60cba2aff0ef
Brandon Harrington <bh68484@gmail.com>
2018-06-21 18:56:42 -0400

    Added user's name to application.

--- traveler/client/src/App.js
+++ traveler/client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from "axios";
 import Cities from "./components/Cities";
 import Home from "./components/Home";
 import SignIn from "./components/SignIn/SignIn";
@@ -7,7 +8,7 @@ import NewUser from "./components/NewUser";
 import Trips from "./components/Trips/Trips";
 import CreateTrip from "./components/CreateTrip";
 import Navtabs from "./components/Navtabs";
-import logo from "./logo.svg";
+import Header from "./components/Header";
 import "./App.css";
 
 class App extends Component {
@@ -15,10 +16,70 @@ class App extends Component {
     cities: ["Raleigh", "Paris", "Amsterdam"]
   };
 
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+      user: null
+    };
+    this._logout = this._logout.bind(this);
+    this._login = this._login.bind(this);
+  }
+  componentDidMount() {
+    axios.get("/auth/user").then(response => {
+      console.log(response.data);
+      if (!!response.data.user) {
+        console.log("THERE IS A USER");
+        this.setState({
+          loggedIn: true,
+          user: response.data.user
+        });
+      } else {
+        this.setState({
+          loggedIn: false,
+          user: null
+        });
+      }
+    });
+  }
+
+  _logout(event) {
+    event.preventDefault();
+    console.log("logging out");
+    axios.post("/auth/logout").then(response => {
+      console.log(response.data);
+      if (response.status === 200) {
+        this.setState({
+          loggedIn: false,
+          user: null
+        });
+      }
+    });
+  }
+
+  _login(username, password) {
+    axios
+      .post("/auth/login", {
+        username,
+        password
+      })
+      .then(response => {
+        console.log(response);
+        if (response.status === 200) {
+          // update the state
+          this.setState({
+            loggedIn: true,
+            user: response.data.user
+          });
+        }
+      });
+  }
+
   render() {
     return (
       <Router>
         <div>
+          <Header user={this.state.user} />
           <Route exact path="/" component={Home} />
           <Route exact path="/Home" component={Home} />
           <Route exact path="/Cities" component={Cities} />
--- traveler/models/user-model.js
+++ traveler/models/user-model.js
@@ -1,12 +1,41 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+mongoose.promise = Promise;
 
 //two properties to use in each user record
 const userSchema = new Schema({
   username: String,
   googleId: String,
   thumbnail: String,
-  name: String
+  name: String,
+  local: {
+    username: { type: String, unique: false, required: false },
+    password: { type: String, unique: false, required: false }
+  }
+});
+
+// Define schema methods
+userSchema.methods = {
+  checkPassword: function(inputPassword) {
+    return bcrypt.compareSync(inputPassword, this.local.password);
+  },
+  hashPassword: plainTextPassword => {
+    return bcrypt.hashSync(plainTextPassword, 10);
+  }
+};
+
+// Define hooks for pre-saving
+userSchema.pre("save", function(next) {
+  if (!this.local.password) {
+    console.log("=======NO PASSWORD PROVIDED=======");
+    next();
+  } else {
+    this.local.password = this.hashPassword(this.local.password);
+    next();
+  }
+  // this.password = this.hashPassword(this.password)
+  // next()
 });
 
 //creating the model or collection --> for every record define the structure of the records according to the userSchema
