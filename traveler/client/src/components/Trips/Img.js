65cab056d7146f6039eb5c65a5f27216e15d981b
Rich <rchrdholme@gmail.com>
2018-06-26 17:58:14 -0400

    profile save

--- traveler/client/src/components/Trips/Img.js
+++ traveler/client/src/components/Trips/Img.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import cityPicApi from "../../utils/cityPicApi";
+// import Weather from "./Weather";
 
-const styles = {
+class Img extends Component {
+  state = {
+    city: "",
+    cityPic: ""
+  };
+
+  styles = {
     img: {
         height: "300px",
         width: "100%",
@@ -9,19 +17,50 @@ const styles = {
         marginTop: "10px",
         marginBottom: "10px"
       }
-}
+  };
 
-const Img = props => {
-  
-        return(
-            <div>
-                <img
-                style={styles.img}
-                src={props.href}
-                />
-            </div>
-        )
-    
+  componentDidMount() {
+    const city = this.props.city;
+    console.log(city);
+    let cityLower = city.toLowerCase();
+    if (cityLower === "san francisco") {
+      cityLower = "san francisco bay area";
+    } else if (cityLower === "wasington, d.c.") {
+      cityLower = "washington d.c.";
+    }
+    const cityDashes = cityLower.replace(/\s+/g, "-");
+
+    this.getCityPic(cityDashes);
+  }
+
+  getCityPic = city => {
+    cityPicApi
+      .cityPic(city)
+      .then(data => this.setState({ cityPic: data.data.photos[0].image.web }))
+      .then(console.log(this.state.cityPic))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    // const imgStyle = { backgroundImage: `url(${this.state.cityPic})` };
+    // return (
+    //   <div className="jumbotron jumbotron-fluid" style={imgStyle}>
+    //     <div className="container">
+    //       {/* <h1 className="display-4 text-center" style={this.styles.cityText}>
+    //         {this.props.city}
+    //       </h1> */}
+    //     </div>
+    //   </div>
+    // );
+    return(
+        <div>
+            <img
+                style={this.styles.img}
+                src={this.state.cityPic}
+            />
+        </div>
+    )
+  }
 }
 
 export default Img;
