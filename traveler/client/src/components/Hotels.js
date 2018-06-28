4abf35ec791a54d4b58f7950e6d9c9bd6196cd8d
James Keith <jamiek531@gmail.com>
2018-06-28 18:59:30 -0400

    Changes to API Routing again

--- traveler/client/src/components/Hotels.js
+++ traveler/client/src/components/Hotels.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 let userId = localStorage.getItem("userId");
-let legId = localStorage.getItem("tripLegId"); 
+let legId = localStorage.getItem("tripLegId");
 console.log("legId " + legId);
 
 const styles = {
@@ -12,7 +12,7 @@ const styles = {
     // borderWidth: 7,
     // borderRadius: "5px",
     // boxShadow: "0 7px 8px 0 rgba(0,0,0,0.2)",
-    boxShadow: "1px 3px 8px 1px #888888",
+    boxShadow: "1px 3px 8px 1px #888888"
     // transition: "0.3s"
   },
   icon: {
@@ -25,7 +25,7 @@ const styles = {
     paddingLeft: 80
   },
   header: {
-    boxShadow: "1px 1px 4px 1px #888888",
+    boxShadow: "1px 1px 4px 1px #888888"
   },
   button: {
     background: "black",
@@ -35,27 +35,21 @@ const styles = {
   }
 };
 
-
 class Hotels extends Component {
-
   sendHotel = (name, address, key) => {
     console.log("click seen on Hotel");
-    console.log(name, address, key);
-  
+    console.log(name, address, "Key:", key);
+
     axios
       .post("/api/trips/addShelter/" + legId, {
         name: name,
         address: address,
-        hotelId: key,
+        hotelId: key
       })
       .then(function(response) {
         console.log(response);
         axios
-          .post("api/shelter/addGuest/" + userId +"/"+ key, {
-            name: name,
-            address: address,
-            hotelId: key,
-          })
+          .put("/api/shelter/addGuest/" + userId + "/" + key)
           .then(function(response) {
             console.log(response);
           })
@@ -83,18 +77,22 @@ class Hotels extends Component {
                 style={styles.icon}
                 className="col-md-1 col-sm-2 col-xs-2"
               />
-              <div className="card-header col-md-11 col-sm-10 col-xs-10" style={styles.header}>
+              <div
+                className="card-header col-md-11 col-sm-10 col-xs-10"
+                style={styles.header}
+              >
                 <h2 id={this.props.name}>{this.props.name}</h2>
                 <h4 id={this.props.address}>{this.props.address}</h4>
               </div>
             </div>
             <div className="card-body text-dark">
               <h3 className="card-title" style={styles.rating}>
-                Rating: {this.props.rating ? this.props.rating : "None Provided"}
+                Rating:{" "}
+                {this.props.rating ? this.props.rating : "None Provided"}
               </h3>
               <p className="card-text text-left" style={styles.rating}>
-                This is where we'll display those in our DB who have stayed here/are
-                staying here
+                This is where we'll display those in our DB who have stayed
+                here/are staying here
               </p>
             </div>
             <button
@@ -104,7 +102,13 @@ class Hotels extends Component {
               className="btn addBtn"
               dataname={this.props.name}
               dataaddress={this.props.address}
-              onClick={() => this.sendHotel(this.props.name, this.props.address, this.props.hotelId)}
+              onClick={() =>
+                this.sendHotel(
+                  this.props.name,
+                  this.props.address,
+                  this.props.hotelId
+                )
+              }
               // {((e) => this.handleClick(e, data)) }
             >
               Add to My Path
@@ -125,8 +129,8 @@ class Hotels extends Component {
           </div>
         </div>
       </div>
-    
-  )}
+    );
+  }
 }
 
 export default Hotels;
