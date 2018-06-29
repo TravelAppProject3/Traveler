a0b30fd3f0f4ea0b9f62119c39b681c620844cb0
James Keith <jamiek531@gmail.com>
2018-06-28 20:30:51 -0400

    JSON LOCK file added

--- traveler/client/src/components/Restaurant.js
+++ traveler/client/src/components/Restaurant.js
@@ -11,7 +11,7 @@ const styles = {
     // borderWidth: 7,
     // borderRadius: "5px",
     // boxShadow: "0 7px 8px 0 rgba(0,0,0,0.2)",
-    boxShadow: "1px 3px 8px 1px #888888",
+    boxShadow: "1px 3px 8px 1px #888888"
     // transition: "0.3s"
   },
   icon: {
@@ -24,7 +24,7 @@ const styles = {
     paddingLeft: 80
   },
   header: {
-    boxShadow: "1px 1px 4px 1px #888888",
+    boxShadow: "1px 1px 4px 1px #888888"
   },
   button: {
     background: "black",
@@ -54,17 +54,27 @@ const styles = {
 // };
 
 class Restaurant extends Component {
-
   sendRestaurant = (name, address, id) => {
     console.log("click works on Restaurants");
-//   console.log(name, address, id);
-  
+    //   console.log(name, address, id);
+
     axios
       .post("/api/trips/addActivity/" + legId, {
         name: name,
         address: address,
         restaurantBoolean: true,
         activityId: id
+      })
+      .then(function(response) {
+        console.log(response);
+        axios
+          .put("/api/activity/addParticipant/" + userId + "/" + id)
+          .then(function(response) {
+            console.log(response);
+          })
+          .catch(function(error) {
+            console.log("Err - " + error);
+          });
       })
       .then(function(response) {
         console.log(response);
@@ -84,7 +94,7 @@ class Restaurant extends Component {
       .catch(function(error) {
         console.log("Err - " + error);
       });
-    };
+  };
 
   render() {
     return (
@@ -101,18 +111,22 @@ class Restaurant extends Component {
                 style={styles.icon}
                 className="col-md-1 col-sm-2 col-xs-2"
               />
-              <div className="card-header col-md-11 col-sm-10 col-xs-10" style={styles.header}>
+              <div
+                className="card-header col-md-11 col-sm-10 col-xs-10"
+                style={styles.header}
+              >
                 <h2>{this.props.name}</h2>
                 <h4>{this.props.address}</h4>
               </div>
             </div>
             <div className="card-body text-dark">
               <h3 className="card-title" style={styles.rating}>
-                Rating: {this.props.rating ? this.props.rating : "None Provided"}
+                Rating:{" "}
+                {this.props.rating ? this.props.rating : "None Provided"}
               </h3>
               <p className="card-text text-left" style={styles.rating}>
-                This is where we'll display those in our DB who have stayed here/are
-                staying here
+                This is where we'll display those in our DB who have stayed
+                here/are staying here
               </p>
             </div>
             <button
@@ -120,7 +134,11 @@ class Restaurant extends Component {
               className="btn addBtn"
               style={styles.button}
               onClick={() =>
-                this.sendRestaurant(this.props.name, this.props.address, this.props.restaurantId)
+                this.sendRestaurant(
+                  this.props.name,
+                  this.props.address,
+                  this.props.restaurantId
+                )
               }
             >
               Add to My Path
@@ -143,7 +161,8 @@ class Restaurant extends Component {
           </div>
         </div>
       </div>
-  )}
+    );
+  }
 }
 
 export default Restaurant;
